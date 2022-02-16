3e44cc52f84b02e49937a655da7bdb2a583cdc79
Dzmitry Buraukin <Dzmitry.Buraukin@itechart-group.com>
2022-02-16 16:51:21 +0300

    translate text

--- src/pages/AboutUs/components/AboutUsDescription/index.js
+++ src/pages/AboutUs/components/AboutUsDescription/index.js
@@ -10,9 +10,9 @@ const AboutUsDescription= () => {
       <Col span={12} offset={6}>
         <div>
           <Text>
-            Сервис позволит вести общую базу автомобилей где пользователи смогут просмотреть историю технического обслуживания автомобиля, создать новый элемент истории или отредактировать имеющийся.
-            Это поможет избежать обмана при продаже автомобиля, например махинации с пробегом, так как в истории будет видно когда и на каком пробеге производилось обслуживание. 
-            Также поможет владельцу автомобиля вести учет тех обслуживания и в дальнейшем предсказать поломку или время следующего обслуживания.
+            The service will allow you to maintain a common database of cars where users can view the history of car maintenance, create a new history element or edit an existing one.
+            This will help to avoid fraud when selling a car, such as fraud with mileage, since the history will show when and at what mileage the service was performed.
+            It will also help the owner of the car to keep a record of those services and in the future to predict a breakdown or the time of the next service.
           </Text>
         </div>
         <div className="description-links">
